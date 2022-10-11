5430b8af5b73ffeac4abdfc5d41ee11bbdf4109a
de Lamirande <W0443703@campus.nscc.ca>
2022-10-11 08:57:00 -0300

    Initial commit

---
+++ Conditionals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditinals");
+            Console.WriteLine("-----------");
+            Console.WriteLine();
+
+            int score;
+            string gamertag;
+
+            score = 0;
+            gamertag = "BaconBoy3142";
+            if (gamertag == "BaconBoy3142")
+            {
+                score = 1000000;
+            }
+            Console.WriteLine("Score: " + score);
+
+            Console.ReadKey(true);
+        }
+    }
+}
